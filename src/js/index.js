e514dc24e3c1312797bb38d30ea4a0c19d80cf8c
thmsbfft <thmsbfft@gmail.com>
2016-01-29 11:33:35 -0800

    started building website

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	console.log('👻');
 	App.init(document.getElementById('container'));
 
+	window.addEventListener("click", function(e) {
+		console.log(Math.random());
+	});
+
+
 });
