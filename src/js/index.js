bd02272efacd610627afb5cf8a76dfcadeb8268a
thmsbfft <thmsbfft@gmail.com>
2016-05-18 18:04:07 -0700

    Trim a bunch of things

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,4 @@ document.addEventListener("DOMContentLoaded", function() {
 	console.log('👻');
 	App.init(document.getElementById('container'));
 
-	window.addEventListener("click", function(e) {
-		console.log(Math.random());
-	});
-
-
 });
